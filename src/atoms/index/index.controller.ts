c6a172017cf6522d3c593060188fcfe11e1c3c60
Mohammad-Ali A'RÂBI <aerabi@gmx.de>
2021-02-14 17:07:12 +0100

    Add models to the index controller

--- src/atoms/index/index.controller.ts
+++ src/atoms/index/index.controller.ts
@@ -1,10 +1,11 @@
-import { Controller, Body, Get, Path, Post, Route, SuccessResponse } from 'tsoa';
+import { Body, Controller, Path, Post, Route, SuccessResponse } from 'tsoa';
+import { MessageCreationRequest } from "./data/index.models";
 
 @Route('')
 export class IndexController extends Controller {
     @SuccessResponse("201", "Created")
     @Post('user/{userId}/msg')
-    public async createMessage(@Path() userId: string, @Body() requestBody: object) {
+    public async createMessage(@Path() userId: string, @Body() requestBody: MessageCreationRequest) {
         return { userId, body: requestBody };
     }
 }
