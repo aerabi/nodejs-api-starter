2beea3465343855174c24bf94408b5f4aa57bc0b
Mohammad-Ali A'RÂBI <aerabi@gmx.de>
2021-02-14 16:51:40 +0100

    Add body parser to TSOA

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,14 @@
 import * as express from "express";
+import * as bodyParser from "body-parser";
 import { RegisterRoutes } from "./routes/routes";
 
 export const app = express();
+
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+app.use(bodyParser.json());
 
 RegisterRoutes(app);
